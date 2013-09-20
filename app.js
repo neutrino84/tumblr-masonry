deccd29d4b66d6f7bda6cb6b4f02b738d6fa39f0
Oliver Dyla <odyla@Olivers-MacBook-Pro.local>
2013-09-19 19:43:27 -0700

    updated port binding

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express'),
     app = express();
 
 app.configure(function() {
-    app.set('port', 3000);
+    app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'html');
     app.set('layout', 'layout');
